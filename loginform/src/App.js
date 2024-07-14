e0b41ed36f651840a78e0d7cb127908206e96fb3
강준영 <temp@tempui-MacBookAir.local>
2024-07-14 17:10:11 +0900

    714

--- loginform/src/App.js
+++ loginform/src/App.js
@@ -2,30 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group'; //화면전환 애니메이션 라이브러리
 import LoginForm from './Component/LoginForm/LoginForm';
-import Register from './Component/LoginForm/register';
 import './App.css'; // 애니메이션 스타일 정의를 위해 CSS 파일을 추가해줘
-
 
 function App() {
   const location = useLocation();
 
   return (
     <TransitionGroup>
-    <CSSTransition key={location.key} classNames="page" timeout={500}>
-        <Routes location={location}>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-        </CSSTransition>
+      <LoginForm />
     </TransitionGroup>
   );
-};
+}
 
 const AppWrapper = () => (
   <Router>
-    <App/>
+    <App />
   </Router>
 );
-
 
 export default AppWrapper;
--- loginform/src/Component/LoginForm/LoginForm.jsx
+++ loginform/src/Component/LoginForm/LoginForm.jsx
@@ -1,34 +1,86 @@
-import React from 'react';
-import './LoginForm.css'
-import { FaUser, FaLock } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import './LoginForm.css';
+import { FaUser, FaLock, FaEnvelope } from 'react-icons/fa';
 
 const LoginForm = () => {
+  const [action, setAction] = useState('');
+
+  const registerLink = () => {
+    setAction(' active');
+  };
+
+  const loginLink = () => {
+    setAction('');
+  };
+
   return (
-    <div className = 'wrapper'>
+    <div className={`wrapper${action}`}>
+      <div className="form-box login">
         <form action="">
-            <h1>로그인</h1>
-                <div className='input-box'>
-                    <input type="text" placeholder='아이디' required />
-                    <FaUser className='icon'/>
-                </div>
-                <div className='input-box'>
-                    <input type="password" placeholder='비밀번호' required />
-                    <FaLock className='icon'/>
-                </div>
-                <div className="remember-forgot">
-                    <label><input type="checkbox" />계정 기억하기</label>
-                    <a href="#">비밀번호를 잊으셨나요?</a>
-                </div>
-                <button type='submit'>Login</button>
+          <h1>로그인</h1>
+          <div className="input-box">
+            <input type="text" placeholder="아이디" required />
+            <FaUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="비밀번호" required />
+            <FaLock className="icon" />
+          </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" />
+              계정 기억하기
+            </label>
+            <a href="#">비밀번호를 잊으셨나요?</a>
+          </div>
+          <button type="submit">Login</button>
 
-                <div className="register-link">
-                    <p>아직 계정이 없으신가요? <Link to="/register">가입하기</Link> </p>
-                </div>
+          <div className="register-link">
+            <p>
+              아직 계정이 없으신가요?{' '}
+              <a href="#" onClick={registerLink}>
+                가입하기
+              </a>{' '}
+            </p>
+          </div>
         </form>
+      </div>
 
+      <div className="form-box register">
+        <form action="">
+          <h1>회원가입</h1>
+          <div className="input-box">
+            <input type="text" placeholder="아이디" required />
+            <FaUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="text" placeholder="이메일" required />
+            <FaEnvelope className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="비밀번호" required />
+            <FaLock className="icon" />
+          </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" />
+              나는 가입 약관에 동의합니다<div className=""></div>
+            </label>
+          </div>
+          <button type="submit">가입하기</button>
+
+          <div className="register-link">
+            <p>
+              이미 계정이 있으신가요?{' '}
+              <a href="#" onClick={loginLink}>
+                로그인하기
+              </a>{' '}
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default LoginForm;
